a39fe40f867dc987381bb3f74d6fdebaf9bb4548
Muhammad Anser Naseer <anser@Muhammads-MacBook-Pro.local>
2022-10-11 13:55:51 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Facial Authentication
+
+A simple repository developed in Reactjs and Vite to demonstrate how to implement Facial Authentication using FaceIO. FaceIO is an easy to implement, secure, and adaptable Passwordless Authentication & Authorization framework for web applications powered by Face Recognition.
+
+# What is Facial Recognition
+
+Facial recognition is one of the latest authentication techniques, and many developers are adopting it these days. Facial recognition reduces the hassle of entering your email-password or any other user credentials to log in to a web application.
+
+Facial recognition technology uses artificial intelligence to figure out the unique facial details of a user and store them as a random hash to reduce privacy-related issues.
+
+
+## Packages Used
+
+1. FaceIO (https://faceio.net) :: Facial Authentication for the Web
+2. Vite (https://vitejs.dev) :: Next Generation Frontend Tooling
+
+## Setting up
+
+1. Clone the repository.
+2. Install NPM packages: `npm i`.
+3. Run the project: `npm run dev`.
+
+## Author
+
+Muhammad Anser Naseer (muhammadin87@gmail.com)
---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  let faceio;
+  useEffect(() => {
+    faceio = new faceIO("fioa4ca8");
+  }, []);
+
+  const handleRegister = async () => {
+    try {
+      let response = await faceio.enroll({
+        locale: "auto",
+        payload: { // can be anything that we want to associate with the registered user
+          email: "muhammadin87@gmail.com",
+          pin: "1234",
+        },
+      });
+
+      console.log(`All the Details of returned data: ${JSON.stringify(response)}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogIn = async () => {
+    try {
+      let response = await faceio.authenticate({
+        locale: "auto",
+      });
+
+      console.log(`All the Details of returned data: ${JSON.stringify(response)}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <section>
+      <h1>Face Authentication example using FaceIO</h1>
+      <button onClick={handleRegister}>Register your Face</button>
+      <button onClick={handleLogIn}>Log In</button>
+    </section>
+  );
+}
+
+export default App;
